c195bfda780ea9b9b06b4a9a04fb5f2a7e4559fe
Madhura Kulkarni <madhura.kulkarni12353@gmail.com>
2017-03-29 19:10:56 -0400

    removed unwanted files

--- Input validation with  function and libraries/Input validation with  function and libraries/validation header.h
+++
@@ -1,9 +0,0 @@
-
-void printBanner();
-//This function used for input validation when user give letters insted of numbers
-int getInteger();
-double getNumber();
-int getMultipleOf(int input);
-char getLetter();
-double getNumInRange(int min, int max);
-int getPerfectSquare();
--- README.md
+++
@@ -1,2 +0,0 @@
-# introductiontocpp
-This repository contains all the assigments that I did for CIT 120 course work.
