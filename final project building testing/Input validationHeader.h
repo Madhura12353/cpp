b4391431dabea95ae16243d474764c33d61c1eb7
Madhura Kulkarni <madhura.kulkarni12353@gmail.com>
2017-03-29 19:07:47 -0400

    removed unwanted files

--- final project building testing/Input validationHeader.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-//const int SIZE = 70;
-double getNumber();
-int getInteger();
-int getPositiveInteger();
-int getIntInrange(int min, int max);
-int getBaseInRange(int min, int max);
-
-
